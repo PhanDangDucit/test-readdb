2e64db2e87398d0b34f4fd599286c50b456564a4
phandangducit <phanduc.flp@gmail.com>
2024-03-19 17:21:44 +0700

    check bug pagination

--- app/ui/blog/pagination.tsx
+++ app/ui/blog/pagination.tsx
@@ -71,7 +71,7 @@ export default function Pagination(
                             }
                             {/* TH3 */}
                             {
-                                totalPages >= currentPage && currentPage >= currentPage-2 && (
+                                totalPages >= currentPage && currentPage >= totalPages-2 && (
                                     <>
                                         <li>
                                             <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
